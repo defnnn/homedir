81f087652d1e1134068f5ecc38cdc836023b8071
Amanibhavam <iam@defn.sh>
2021-06-07 20:25:02 -0700

    npm install @cdktf/provider-aws

--- p/tf/main.ts
+++ p/tf/main.ts
@@ -1,15 +1,18 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
 
+import { AwsProvider } from "./.gen/providers/aws";
+
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
-
+    new AwsProvider(this, "aws", {
+      region: "us-east-1",
+    });
   }
 }
 
 const app = new App();
-new MyStack(app, 'tf');
+new MyStack(app, 'defn');
 app.synth();
