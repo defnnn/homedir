0140ca6cb4e9093e6b9768b5b9c58a47cb5966cb
Amanibhavam <iam@defn.sh>
2021-06-07 21:07:54 -0700

    make two stacks with typescript

--- p/tf/main.ts
+++ p/tf/main.ts
@@ -1,7 +1,7 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
 
-import { AwsProvider } from "./.gen/providers/aws";
+import { AwsProvider, Instance } from "./.gen/providers/aws";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -10,9 +10,15 @@ class MyStack extends TerraformStack {
     new AwsProvider(this, "aws", {
       region: "us-east-1",
     });
+
+    new Instance(this, "Hello", {
+      ami: "ami-2757f631",
+      instanceType: "t2.nano",
+    });
   }
 }
 
 const app = new App();
 new MyStack(app, 'defn');
+new MyStack(app, 'defm');
 app.synth();
