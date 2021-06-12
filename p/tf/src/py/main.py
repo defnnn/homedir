a2fa5733d5dc5a547c3db6b00d3e318e99cfd178
Amanibhavam <iam@defn.sh>
2021-06-12 11:58:46 -0700

    examples for ts, py

---
+++ p/tf/src/py/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from constructs import Construct
+from cdktf import App, TerraformStack
+
+
+from imports.aws import AwsProvider, Instance
+
+
+class MyStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
+
+        AwsProvider(self, 'Aws', region='us-east-1')
+
+        helloInstance = Instance(self, 'hello',
+        ami="ami-2757f631",
+        instance_type="t2.nano",
+        )
+
+app = App()
+MyStack(app, "default")
+
+app.synth()
