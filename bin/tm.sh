05407ca8090f82084aeb0bd346000b8902c42604
Amanibhavam <iam@defn.sh>
2021-12-21 22:01:53 -0800

    tm no longer handles the gpg stuff

--- bin/tm.sh
+++ bin/tm.sh
@@ -3,7 +3,7 @@ unset TMUX;
 mkdir -p "${HOME}/.ssh"; 
 ln -nfs "${SSH_AUTH_SOCK}" "${HOME}/.ssh/ssh_auth_sock"; 
 export SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock";
-pth_tmux="$(which /home/linuxbrew/.linuxbrew/bin/tmux tmux | head -1)";
+pth_tmux="$(which tmux | head -1)";
 "$pth_tmux" new-session -d -s default || true;
 "$pth_tmux" detach-client -s default || true;
 exec "$pth_tmux" -CC -u attach -t default;
